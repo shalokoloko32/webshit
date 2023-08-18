e2942010a360eaf986cd20c6507758d09ace026c
BraderAngelo <95712581+BraderAngelo@users.noreply.github.com>
2023-08-19 00:36:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const videoPlayer = document.getElementById("video-player");
+
+searchButton.addEventListener("click", () => {
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm !== "") {
+    searchVideos(searchTerm);
+  }
+});
+
+function searchVideos(query) {
+  // Replace 'YOUR_API_KEY' with your actual YouTube API key
+  const apiKey = 'AIzaSyBiKXMadQYe0SNzn8j8UNeQcotsF8OnyPY';
+  const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKey}&maxResults=1`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const videoId = data.items[0].id.videoId;
+      playVideo(videoId);
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
+    });
+}
+
+function playVideo(videoId) {
+  videoPlayer.src = `https://www.youtube.com/embed/${videoId}`;
+}
